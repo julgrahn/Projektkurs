465456682ffdbe78cefd2319f27afef05977e95a
jedeland10 <johan.edeland@live.com>
2021-04-15 16:07:31 +0200

    Ny folder o main

---
+++ topdown/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//#include <SDL.h>
+//#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+
+int main(int argc, char* args[]){
+    printf("hej");
+
+    return 0;
+}
