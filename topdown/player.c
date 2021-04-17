3e32caab0aebe69eaacbe90749495701dc796ace
kvart32 <isacsson7@gmail.com>
2021-04-17 19:30:40 +0200

    Städade main.c och la till rect i playerstructen

--- topdown/player.c
+++ topdown/player.c
@@ -1,22 +1,39 @@
 #include <stdlib.h>
 #include "player.h"
+#include <SDL.h>
 
 #define PUBLIC
-#define SPEED 1
-struct Player_type{
+#define SPEED 10
+
+struct Player_type {
     int health;
-    int positionX;
-    int positionY;
     int speed;
+    SDL_Rect pDimensions;
 };
 
 PUBLIC Player createPlayer(int x, int y)
 {
     Player a = malloc(sizeof(struct Player_type));
     a->health = 100;
-    a->positionX = x;
-    a->positionY = y;
     a->speed = SPEED;
-    
+
+    a->pDimensions.x = x;
+    a->pDimensions.y = y;
+    a->pDimensions.w = 64;
+    a->pDimensions.h = 64;
+
     return a;
+}
+
+PUBLIC void movePlayer(Player p, int up, int down, int right, int left)
+{
+    if (up && !down) p->pDimensions.y -= SPEED;
+    if (down && !up) p->pDimensions.y += SPEED;
+    if (left && !right) p->pDimensions.x -= SPEED;
+    if (right && !left) p->pDimensions.x += SPEED;
+}
+
+PUBLIC SDL_Rect getPlayerRect(Player p)
+{
+    return p->pDimensions;
 }
