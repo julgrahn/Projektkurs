264408f59754dc5fa83e568e7317c5deec876bb1
jedeland10 <johan.edeland@live.com>
2021-04-19 17:31:06 +0200

    merge

--- topdown/world.h
+++ topdown/world.h
@@ -8,10 +8,6 @@ int getTileRows();
 int getTileColumns();
 int getTileWidth();
 int getTileHeight();
-<<<<<<< HEAD
-int getTileGrid(int x,int y);
-=======
 int getTileGrid(int x, int y);
->>>>>>> e00810607ca7030f068fa815f3bb1e47fe9a5eb2
 
 #endif
