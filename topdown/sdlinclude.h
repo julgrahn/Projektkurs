d60e48fa91067e1cc3f8f2fdd93558766f10a230
julgrahn <alexander.grahn@hotmail.se>
2021-04-23 10:40:37 +0200

    fix

--- topdown/sdlinclude.h
+++ topdown/sdlinclude.h
@@ -1,12 +1,12 @@
 #ifndef SDLINCLUDE_H
 #define SDLINCLUDE_H
 
-// #include <SDL.h>
-// #include <SDL_image.h>
-// #include <SDL_timer.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_timer.h>
+//#include <SDL2/SDL.h>
+//#include <SDL2/SDL_image.h>
+//#include <SDL2/SDL_timer.h>
 
 #define WINDOWWIDTH 704
 #define WINDOWHEIGHT 704
