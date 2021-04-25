fd2fe915bea42b295bab4cc29f40a69ca12561ee
kvart32 <isacsson7@gmail.com>
2021-04-25 15:15:43 +0200

    Update main.c

--- topdown/main.c
+++ topdown/main.c
@@ -127,7 +127,7 @@ void renderGame(SDL_Renderer* renderer, SDL_Texture* mTiles, SDL_Rect gTiles[], 
     SDL_Texture* bulletTexture, Player players[], SDL_Texture* playerText, SDL_Rect playerRect[], SDL_Point* playerRotationPoint)
 {
     SDL_RenderClear(renderer);
-
+    // Render Background
     SDL_Rect position;
     position.y = 0;
     position.x = 0;
@@ -143,7 +143,7 @@ void renderGame(SDL_Renderer* renderer, SDL_Texture* mTiles, SDL_Rect gTiles[], 
             SDL_RenderCopyEx(renderer, mTiles, &gTiles[getTileGrid(i, j)], &position, 0, NULL, SDL_FLIP_NONE);
         }
     }
-
+    // Render Bullets
     for (int i = 0; i < MAX_BULLETS; i++)
     {
         if (isBulletActive(bullets[i]))
@@ -151,7 +151,7 @@ void renderGame(SDL_Renderer* renderer, SDL_Texture* mTiles, SDL_Rect gTiles[], 
             SDL_RenderCopy(renderer, bulletTexture, NULL, getBulletRect(bullets[i]));
         }
     }
-
+    // Render Players
     for (int i = 0; i < MAX_PLAYERS; i++)
     {
         SDL_RenderCopyEx(renderer, playerText, &playerRect[getPlayerFrame(players[i])], getPlayerRect(players[i]), getPlayerDirection(players[i]), playerRotationPoint, SDL_FLIP_NONE);
