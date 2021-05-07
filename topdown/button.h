f51d57772528f1a261079953ff4632dd3acd5bf4
julgrahn <alexander.grahn@hotmail.se>
2021-05-07 13:43:13 +0200

    start to main menu

---
+++ topdown/button.h
@@ -0,0 +1,13 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "sdlinclude.h"
+#include <stdio.h>
+
+enum ButtonState { NORMAL = 0, HOVER = 1, PRESSED = 2 };
+
+
+
+
+
+#endif
---
+++ topdown/menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+#include "sdlinclude.h"
+#include <stdio.h>
+
+
+
+
+
+
+#endif MENU_H
